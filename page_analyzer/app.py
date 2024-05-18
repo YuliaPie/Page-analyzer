d553aedce7b971f053daae5117f7334620114208
Yulia <jilly-2005@yandex.ru>
2024-05-18 21:28:43 +0300

    1-11

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,11 +1,6 @@
 from flask import Flask
-from dotenv import load_dotenv
-
-
-load_dotenv()
 app = Flask(__name__)
 
-
 @app.route('/')
-def welcome():
-    return 'Welcome to Flask!'
+def hello_world():
+    return 'Hello, World!'
