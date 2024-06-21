a77c9e5ffa63e4b51b375d1733c03f24e403f67f
Yulia <jilly-2005@yandex.ru>
2024-06-21 14:58:16 +0300

    changed database.sql

--- database.sql
+++ database.sql
@@ -1,10 +1,11 @@
+drop table if exists urls;
 CREATE TABLE IF NOT EXISTS urls
 (
   url_id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   name VARCHAR(50) NOT NULL,
   created_at DATE NOT NULL
 );
-
+drop table if exists url_checks;
 CREATE TABLE IF NOT EXISTS url_checks
 (
   check_id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
